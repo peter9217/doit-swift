b1010449940034a59197b9737f987b11de314548
장반석 <jang@jangbanseog-ui-MacBookAir.local>
2022-06-01 14:03:03 +0900

    first

---
+++ doit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  doit
+//
+//  Created by 장반석 on 2022/06/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ doit/doitApp.swift
@@ -0,0 +1,17 @@
+//
+//  doitApp.swift
+//  doit
+//
+//  Created by 장반석 on 2022/06/01.
+//
+
+import SwiftUI
+
+@main
+struct doitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
